d0a6b37ec5e2280f44472e90b48ad929e46b2c01
haimnewclass <haim@newclass.co.il>
2022-04-26 20:11:17 +0300

    Start

---
+++ Lehava.Tasks.Entities/File.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lehava.Tasks.Entities
+{
+    internal class File
+    {
+        public void Run()
+        {
+            string fileName = @"C:\a\1.txt";
+            // create file
+            System.IO.File.WriteAllText(fileName, "*");
+        }
+    }
+}
